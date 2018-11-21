8f18b1f7bd6949ee81066738d4964af25af20d3c
Alex Buznik <shu@buznik.net>
2018-11-21 15:40:09 +0200

    Add posts and categories

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @content_first = 'Sample paragraph'
+    @content_second = 'Another paragraph'
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end
