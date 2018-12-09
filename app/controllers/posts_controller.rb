7438b2703be4c244533bea91e71c411d34eb807d
Alex Buznik <shu@buznik.net>
2018-12-09 15:47:15 +0200

    Update and delete posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,19 +4,32 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
+    @category = Category.all
   end
 
   def edit
-
+    @post = Post.find(params[:id])
+    @category = Category.all
   end
 
   def create
-
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path, :notice => "Your post has been saved"
+    else
+      render "new"
+    end
   end
 
   def update
+    @post = Post.find(params[:id])
 
+    if @post.update_attributes(params[:post])
+      redirect_to post_path, :notice => "Post updated"
+    else
+      render "edit"
+    end
   end
 
   def show
@@ -24,6 +37,8 @@ class PostsController < ApplicationController
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, :notice => "Post deleted"
   end
 end
