bbf621c485cd50f9911f5b36d619c5a01c05e8b1
Alex Buznik <shu@buznik.net>
2018-12-14 14:24:47 +0200

    Display user name for posts and in the admin

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :author_id, :body, :category_id, :title
+  attr_accessible :author_id, :body, :category_id, :title, :admin_user_id
   belongs_to :category
   belongs_to :admin_user
 end
